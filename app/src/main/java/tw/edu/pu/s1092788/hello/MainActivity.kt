64173538cb7ee5ce80d26b7c95e209e8539d9642
tako0630 <ego37339728@gmail.com>
2022-04-10 01:22:15 +0800

    合併後，再次修改txv顯示的文字

--- app/src/main/java/tw/edu/pu/s1092788/hello/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/s1092788/hello/MainActivity.kt
@@ -2,10 +2,19 @@ package tw.edu.pu.s1092788.hello
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var txv: TextView = findViewById<TextView>(R.id.txv)
+        //txv.text = "Hello tcyang"
+        //TestDEV()
+        //TestMaster()
+        txv.text = "嗨，我是楊子青"
+
     }
+
 }
